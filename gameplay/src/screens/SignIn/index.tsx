6198c42c1822463267207beb66007ee961f8cff2
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-07-21 23:23:30 -0300

    Tela inicial.

--- gameplay/src/screens/SignIn/index.tsx
+++ gameplay/src/screens/SignIn/index.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { View } from 'react-native';
+import { 
+  View,
+  Text,
+  Image,
+  StatusBar
+} from 'react-native';
 
+import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
 
 export function SignIn() {
   return (
-    <View />
+    <View style={styles.container}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {'\n'} 
+          suas jogatinas {'\n'} 
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'}
+          favoritos com seus amigos.
+        </Text>
+      </View>
+    </View>
   );
 }
