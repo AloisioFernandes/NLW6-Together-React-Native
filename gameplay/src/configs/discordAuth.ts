f5b074f45c03418f50810b77faab1f422360a527
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-08-15 01:58:13 -0300

    Definindo rotas públicas e privadas.

--- gameplay/src/configs/discordAuth.ts
+++ gameplay/src/configs/discordAuth.ts
@@ -1,4 +1,4 @@
-const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2Fgameplay'
+const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-78db2a87-aecf-4c54-b4b7-b21ca38aed56'
 const SCOPE = 'identify%20email%20connections%20guilds'
 const RESPONSE_TYPE = 'token'
 const CLIENT_ID = '876259014248394813'
--- gameplay/src/hooks/auth.tsx
+++ gameplay/src/hooks/auth.tsx
@@ -19,15 +19,20 @@ type User = {
   token: string;
 }
 
-
-
 type AuthContextData = {
   user: User;
+  loading: boolean;
   signIn: () => Promise<void>;
 }
 
 type AuthProviderProps = {
   children: ReactNode;
+}
+
+type AuthorizationResponse = AuthSession.AuthSessionResult & {
+  params: {
+    access_token: string;
+  }
 }
 
 export const AuthContext = createContext({} as AuthContextData)
@@ -42,16 +47,39 @@ function AuthProvider({ children }: AuthProviderProps) {
 
       const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
 
-      const response = AuthSession.startAsync({ authUrl })
-      console.log(response)
+      const { type, params } = await AuthSession
+      .startAsync({ authUrl }) as AuthorizationResponse
       
+      if(type == "success") {
+        api.defaults.headers.authorization = `Bearer ${params.access_token}`
+
+        let userInfo = await api.get('/users/@me')
+
+        const firstName = userInfo.data.username.split(' ')[0]
+        userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`
+        
+        setUser({
+          ...userInfo.data,
+          firstName,
+          token: params.access_token
+        })
+        setLoading(false)
+      } else {
+        setLoading(false)
+      }
     } catch {
       throw new Error('Não foi possível autenticar')
     }
   }
 
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider 
+      value={{ 
+        user, 
+        signIn, 
+        loading 
+      }}
+    >
       { children }
     </AuthContext.Provider>
   )
